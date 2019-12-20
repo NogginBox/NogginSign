7e2418c8c5654a0f985fd61cd22fb56070b11032
Richard Garside <richard.garside@nogginbox.co.uk>
2019-12-20 07:51:05 +0000

    Converted Counters

---
+++ nogginsign.net/Constants/Counters.cs
@@ -0,0 +1,11 @@
+﻿namespace NogginSign.net.Constants
+{
+    public static class Counters
+    {
+        public const string COUNTER_1 = "\x08z";
+        public const string COUNTER_2 = "\x08{";
+        public const string COUNTER_3 = "\x08|";
+        public const string COUNTER_4 = "\x08}";
+        public const string COUNTER_5 = "\x08-";
+    }
+}
