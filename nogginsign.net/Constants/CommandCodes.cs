7a01112b727cc41afe40e4e37c1b4a3af52f02db
Richard Garside <richard.garside@nogginbox.co.uk>
2019-12-20 07:40:22 +0000

    Moved Constants to CommandCodes and PacketConstants

---
+++ nogginsign.net/Constants/CommandCodes.cs
@@ -0,0 +1,23 @@
+﻿namespace NogginSign.net.Constants
+{
+    public static class CommandCodes
+    {
+        public const string WRITE_TEXT          = "A";  // Write TEXT file (p18)
+        public const string READ_TEXT           = "B";  // Read TEXT file (p19)
+        public const string WRITE_SPECIAL       = "E";  // Write SPECIAL FUNCTION commands (p21)
+        public const string READ_SPECIAL        = "F";  // Read SPECIAL FUNCTION commands (p29)
+        public const string WRITE_STRING        = "G";  // Write STRING (p37)
+        public const string READ_STRING         = "H";  // Read STRING (p38)
+        public const string WRITE_SMALL_DOTS    = "I";  // Write SMALL DOTS PICTURE file (p39)
+        public const string READ_SMALL_DOTS     = "J";  // Read SMALL DOTS PICTURE file (p41)
+        public const string WRITE_RGB_DOTS      = "K";  // Write RGB DOTS PICTURE file (p44)
+        public const string READ_RGB_DOTS       = "L";  // Read RGB DOTS PICTURE file (p46)
+        public const string WRITE_LARGE_DOTS    = "M";  // Write LARGE DOTS PICTURE file (p42)
+        public const string READ_LARGE_DOTS     = "N";  // Read LARGE DOTS PICTURE file (p43)
+        public const string WRITE_ALPHAVISION   = "O";  // Write ALPHAVISION BULLETIN (p48)
+        public const string SET_TIMEOUT         = "T";  // Set Timeout Message (p118) (Alpha 2.0/3.0)
+
+        public const string UNLOCKED            = "U";
+        public const string LOCKED              = "L";
+    }
+}
---
+++ nogginsign.net/Constants/PacketConstants.cs
@@ -0,0 +1,32 @@
+﻿namespace NogginSign.net.Constants
+{
+    /// <summary>
+    /// Constants used in transmission packets
+    /// </summary>
+    public static class PacketConstants
+    {
+        public const string NUL     = "\x00";  // NULL
+        public const string SOH     = "\x01";  // Start of Header
+        public const string STX     = "\x02";  // Start of TeXt (precedes a command code)
+        public const string ETX     = "\x03";  // End of TeXt
+        public const string EOT     = "\x04";  // End Of Transmission
+        //public const string #ENQ    = "\x05"  # Enquiry
+        //public const string #ACK    = "\x06"  # Acknowledge
+        public const string BEL     = "\x07";  // Bell
+        public const string BS      = "\x08";  // Backspace
+        public const string HT      = "\x09";  // Horizontal tab
+        public const string LF      = "\x0A";  // Line Feed
+        public const string NL      = "\x0A";  // New Line
+        public const string VT      = "\x0B";  // Vertical Tab
+        //public const string #FF     = "\x0C"  # Form Feed
+        public const string NP      = "\x0C";  // New Page
+        public const string CR      = "\x0D";  // Carriage Return
+        public const string CAN     = "\x18";  // Cancel
+        public const string SUB     = "\x1A";  // Substitute (select charset)
+        public const string ESC     = "\x1B";  // Escape character
+
+        public const string NEWLINE = NL;
+        public const string NEWPAGE = NP;
+    }
+
+}
