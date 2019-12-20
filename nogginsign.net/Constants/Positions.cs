3fcfb8da747f88b788dcf06dbcf0c5f95af311a9
Richard Garside <richard.garside@nogginbox.co.uk>
2019-12-20 08:21:28 +0000

    Converted positions

---
+++ nogginsign.net/Constants/Positions.cs
@@ -0,0 +1,15 @@
+﻿namespace NogginSign.net.Constants
+{
+    /// <summary>
+    /// Display positions
+    /// </summary>
+    public static class Positions
+    {
+        public const string MIDDLE_LINE = "\x20";
+        public const string TOP_LINE    = "\x22";
+        public const string BOTTOM_LINE = "\x26";
+        public const string FILL        = "\x30";
+        public const string LEFT        = "\x31";
+        public const string RIGHT       = "\x32";
+    }
+}
