476ddb8c3c25ad42ff0ddbc3b836c019bc04fd56
Richard Garside <richard.garside@nogginbox.co.uk>
2019-12-20 08:19:09 +0000

    Converted modes

---
+++ nogginsign.net/Constants/Modes.cs
@@ -0,0 +1,53 @@
+﻿namespace NogginSign.net.Constants
+{
+    class Modes
+    {
+        // Normal display modes
+        public const string ROTATE            = "a";
+        public const string HOLD              = "b";
+        public const string FLASH             = "c";
+        public const string ROLL_UP           = "e";
+        public const string ROLL_DOWN         = "f";
+        public const string ROLL_LEFT         = "g";
+        public const string ROLL_RIGHT        = "h";
+        public const string WIPE_UP           = "i";
+        public const string WIPE_DOWN         = "j";
+        public const string WIPE_LEFT         = "k";
+        public const string WIPE_RIGHT        = "l";
+        public const string SCROLL            = "m";
+        public const string AUTOMODE          = "o";
+        public const string ROLL_IN           = "p";
+        public const string ROLL_OUT          = "q";
+        public const string WIPE_IN           = "r";
+        public const string WIPE_OUT          = "s";
+        public const string COMPRESSED_ROTATE = "t";
+        public const string EXPLODE           = "u";
+        public const string CLOCK             = "v";
+
+        // Special modes
+        public const string TWINKLE           = "n0";
+        public const string SPARKLE           = "n1";
+        public const string SNOW              = "n2";
+        public const string INTERLOCK         = "n3";
+        public const string SWITCH            = "n4";
+        public const string SLIDE             = "n5";
+        public const string SPRAY             = "n6";
+        public const string STARBURST         = "n7";
+        public const string WELCOME           = "n8";
+        public const string SLOT_MACHINE      = "n9";
+        public const string NEWS_FLASH        = "nA";
+        public const string TRUMPET_ANIMATION = "nB";
+        public const string CYCLE_COLORS      = "nC";
+
+        // Special graphics
+        public const string THANK_YOU         = "nS";
+        public const string NO_SMOKING        = "nU";
+        public const string DONT_DRINK_DRIVE  = "nV";
+        public const string RUNNING_ANIMAL    = "nW";
+        public const string FISH_ANIMATION    = "nW";
+        public const string FIREWORKS         = "nX";
+        public const string TURBO_CAR         = "nY";
+        public const string BALLOON_ANIMATION = "nY";
+        public const string CHERRY_BOMB       = "nZ";
+    }
+}
