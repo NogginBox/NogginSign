77d869d8b12146098558399505858c2c8c1baa73
Richard Garside <richard.garside@nogginbox.co.uk>
2019-12-21 17:05:57 +0000

    Working on sign connection

--- nogginsign.net/Exceptions/NogginSignException.cs
+++ nogginsign.net/Exceptions/NogginSignException.cs
@@ -4,10 +4,20 @@ namespace nogginsign.net.Exceptions
 {
     public abstract class NogginSignException : Exception
     {
+        public NogginSignException() : base() {}
+        public NogginSignException(string msg) : base(msg) { }
+        public NogginSignException(string msg, Exception ex) : base(msg, ex) { }
     }
 
     public class SignColourException : NogginSignException
     {
 
+    }
+
+    public class NogginSignConnectionException : NogginSignException
+    {
+        public NogginSignConnectionException() : base() {}
+
+        public NogginSignConnectionException(string msg) : base(msg) {}
     }
 }
---
+++ nogginsign.net/NogginSign.cs
@@ -0,0 +1,33 @@
+﻿using nogginsign.net.Exceptions;
+using System.IO.Ports;
+
+namespace NogginSign.net
+{
+    public class NogginSign
+    {
+        private SerialPort _port;
+
+        public void Connect()
+        {
+            var availablePorts = SerialPort.GetPortNames();
+            _port = new SerialPort();
+
+            if (availablePorts.Length == 0)
+            {
+                throw new NogginSignConnectionException("No COM Port devices found");
+            }
+
+            _port.PortName = availablePorts[0];
+            _port.BaudRate = 38400;
+            //port.DataBits = 5; // 5 - 8
+            //port.StopBits = (StopBits)Enum.Parse(typeof(StopBits), cboStopBits.Text);
+            //port.Handshake = (Handshake)Enum.Parse(typeof(Handshake), cboHandShaking.Text);
+            _port.Parity = Parity.None;
+        }
+
+        public void Send(ISignCommand command)
+        {
+            _port.Write(command.ToString());
+        }
+    }
+}
